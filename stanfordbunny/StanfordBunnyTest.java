94e7a0d21f959e2d29ce54ff0626a2aee1f86bec
Koki Asai <kra.enmn0618.swift@softbank.ne.jp>
2016-07-07 19:35:01 +0900

    throw classpath test, and add library.

---
+++ stanfordbunny/StanfordBunnyTest.java
@@ -0,0 +1,19 @@
+/**
+
+    StanfordBunnyTest
+    
+    @update     2016/07/07
+    @develop    K.Asai ,
+    
+*/
+
+package stanfordbunny;
+
+import com.jogamp.opengl.GL;
+
+public class StanfordBunnyTest extends Object{
+ 
+    
+    
+    
+}
