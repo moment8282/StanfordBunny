6f0519c0b8badf17ee405ef1228bb792fc13573d
SOARA <orihakat.tnsboy.0313@gmail.com>
2016-07-18 03:38:01 +0900

    change Model

--- stanfordbunny/StanfordBunnyModel.java
+++ stanfordbunny/StanfordBunnyModel.java
@@ -13,13 +13,27 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class StanfordBunnyModel extends Object{
     
     private static final String FilePath = "resource/StanfordBunny.ply";     
+    private Double[][] points = new Double[35947][5];
+    private int[][] face = new int[69451][4];
+    private int prNum = 0;
+    private int j = 0; 
+    private int k = 0;
     
     public StanfordBunnyModel(){
-        //this.fileRead(FilePath);
+        this.fileRead(FilePath);
+    }
+    
+    public Double[][] getPoints(){
+        return points;
+    }
+    
+    public int [][] getFace(){
+        return face;
     }
     
     
@@ -32,8 +46,37 @@ public class StanfordBunnyModel extends Object{
 
             String line;
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
+                String[] pts = line.split(" ",0);
+                System.out.println(pts[0]);
+                prNum++;
+                if(pts[0].equals("end_header")){
+                        break;
+                }
+                
             }
+            
+            while ((line = br.readLine()) != null) {
+                String[] pts = line.split(" ",0);
+                if(pts.length == 5){
+                    for(int i=0;i<pts.length;i++){
+                        points[j][i] = Double.parseDouble(pts[i]);
+                    }
+                    j++;
+                }else if(pts.length == 4){
+                    for(int i=0;i<pts.length;i++){
+                        if(i == 0){
+                            face[k][i] = Integer.parseInt(pts[i]); 
+                        }else{
+                            face[k][i] = Integer.parseInt(pts[i])-prNum;
+                        }
+                    }
+                    k++;
+                }
+                
+            }
+            System.out.println("points size = " + points.length );
+            System.out.println("face size = " + face.length );
+            
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
